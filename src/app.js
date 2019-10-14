59827af3756bf20a8210149166be860c7bc2b1f9
Michael Duberstein <mduberst@amazon.com>
2019-10-14 12:10:56 -0400

    Fixed error with multiple firebase initializations

--- src/app.js
+++ src/app.js
@@ -1,29 +1,41 @@
-import React, { Component } from 'react';
-import { View } from 'react-native';
+import React, {Component} from 'react';
+import {View} from 'react-native';
 import firebase from 'firebase';
-import { Header, Button, Spinner } from './components/common';
+import {Header, Button, Spinner, CardSection} from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
-  state = { loggedIn: null };
+  state = {loggedIn: null};
 
-  componentWillMount() {
-    if(!firebase.app.length){
-      firebase.initializeApp({
-        apiKey: 'AIzaSyBtxMY4K6uHxv_2e3GD-FWAD2ACX6lPVRE',
-        authDomain: 'authentication-70a18.firebaseapp.com',
-        databaseURL: 'https://authentication-70a18.firebaseio.com',
-        storageBucket: 'authentication-70a18.appspot.com',
-        messagingSenderId: '682333809338'
-      });
+  componentDidMount() {
+    // Your web app's Firebase configuration
+    const firebaseConfig = {
+      apiKey: 'AIzaSyBPoLkNJUVqt9EW1DyLoGgL6LxO9sEfHic',
+      authDomain: 'auth-ded43.firebaseapp.com',
+      databaseURL: 'https://auth-ded43.firebaseio.com',
+      projectId: 'auth-ded43',
+      storageBucket: 'auth-ded43.appspot.com',
+      messagingSenderId: '938318826372',
+      appId: '1:938318826372:web:8d92e5ce4944ee84eaabd1',
+      measurementId: 'G-05BY1VYGM5',
+    };
+    // Initialize Firebase
+    console.log(
+      `In componentDidMount firebase.app.length: ${firebase.apps.length}`,
+    );
+    if (!firebase.apps.length) {
+      console.log('Before initializing firebase');
+      firebase.initializeApp(firebaseConfig);
     }
 
-
-    firebase.auth().onAuthStateChanged((user) => {
+    // Providing an event handler for both user signing in and signing out
+    // user is an object when a user is signed in
+    // user is a null when a user is signed out
+    firebase.auth().onAuthStateChanged(user => {
       if (user) {
-        this.setState({ loggedIn: true });
+        this.setState({loggedIn: true});
       } else {
-        this.setState({ loggedIn: false });
+        this.setState({loggedIn: false});
       }
     });
   }
@@ -32,9 +44,9 @@ class App extends Component {
     switch (this.state.loggedIn) {
       case true:
         return (
-          <Button onPress={() => firebase.auth().signOut()}>
-            Log Out
-          </Button>
+          <CardSection>
+            <Button onPress={() => firebase.auth().signOut()}>Log Out</Button>
+          </CardSection>
         );
       case false:
         return <LoginForm />;
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,27 +1,31 @@
-import React, { Component } from 'react';
-import { Text } from 'react-native';
+import React, {Component} from 'react';
+import {Text} from 'react-native';
 import firebase from 'firebase';
-import { Button, Card, CardSection, Input, Spinner } from './common';
+import {Button, Card, CardSection, Input, Spinner} from './common';
 
 class LoginForm extends Component {
-  state = { email: '', password: '', error: '', loading: false };
+  state = {email: '', password: '', error: '', loading: false};
 
   onButtonPress() {
-    const { email, password } = this.state;
+    const {email, password} = this.state;
 
-    this.setState({ error: '', loading: true });
+    this.setState({error: '', loading: true});
 
-    firebase.auth().signInWithEmailAndPassword(email, password)
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
       .then(this.onLoginSuccess.bind(this))
       .catch(() => {
-        firebase.auth().createUserWithEmailAndPassword(email, password)
+        firebase
+          .auth()
+          .createUserWithEmailAndPassword(email, password)
           .then(this.onLoginSuccess.bind(this))
           .catch(this.onLoginFail.bind(this));
       });
   }
 
   onLoginFail() {
-    this.setState({ error: 'Authentication Failed', loading: false });
+    this.setState({error: 'Authentication Failed', loading: false});
   }
 
   onLoginSuccess() {
@@ -29,7 +33,7 @@ class LoginForm extends Component {
       email: '',
       password: '',
       loading: false,
-      error: ''
+      error: '',
     });
   }
 
@@ -38,11 +42,7 @@ class LoginForm extends Component {
       return <Spinner size="small" />;
     }
 
-    return (
-      <Button onPress={this.onButtonPress.bind(this)}>
-        Log in
-      </Button>
-    );
+    return <Button onPress={this.onButtonPress.bind(this)}>Log in</Button>;
   }
 
   render() {
@@ -53,7 +53,7 @@ class LoginForm extends Component {
             placeholder="user@gmail.com"
             label="Email"
             value={this.state.email}
-            onChangeText={email => this.setState({ email })}
+            onChangeText={email => this.setState({email})}
           />
         </CardSection>
 
@@ -63,17 +63,13 @@ class LoginForm extends Component {
             placeholder="password"
             label="Password"
             value={this.state.password}
-            onChangeText={password => this.setState({ password })}
+            onChangeText={password => this.setState({password})}
           />
         </CardSection>
 
-        <Text style={styles.errorTextStyle}>
-          {this.state.error}
-        </Text>
+        <Text style={styles.errorTextStyle}>{this.state.error}</Text>
 
-        <CardSection>
-          {this.renderButton()}
-        </CardSection>
+        <CardSection>{this.renderButton()}</CardSection>
       </Card>
     );
   }
@@ -83,8 +79,8 @@ const styles = {
   errorTextStyle: {
     fontSize: 20,
     alignSelf: 'center',
-    color: 'red'
-  }
+    color: 'red',
+  },
 };
 
 export default LoginForm;
